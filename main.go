49f74f6de517b96698326860257d7381ccf57b4d
Zach Robinson <zrobinson@pivotal.io>
2019-01-16 11:40:55 -0800

    add revision command;

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	. "github.com/zrob/cfrevisions-plugin/models"
 	. "github.com/zrob/cfrevisions-plugin/util"
+	"errors"
 )
 
 type CFRevisionsPlugin struct{}
@@ -20,6 +21,13 @@ func (c *CFRevisionsPlugin) Run(cliConnection plugin.CliConnection, args []strin
 			fmt.Println(c.GetMetadata().Commands[0].UsageDetails.Usage)
 		} else {
 			c.showRevisions(cliConnection, args)
+		}
+	}
+	if args[0] == "revision" {
+		if len(args) != 3 {
+			fmt.Println(c.GetMetadata().Commands[1].UsageDetails.Usage)
+		} else {
+			c.showRevisionDetails(cliConnection, args)
 		}
 	}
 }
@@ -40,39 +48,81 @@ func (c *CFRevisionsPlugin) GetMetadata() plugin.PluginMetadata {
 					Usage: "cf revisions APPNAME",
 				},
 			},
+			{
+				Name:     "revision",
+				HelpText: "Display a revision's details",
+				UsageDetails: plugin.Usage{
+					Usage: "cf revision APPNAME VERSION",
+				},
+			},
 		},
 	}
 }
 
 func (c *CFRevisionsPlugin) showRevisions(cliConnection plugin.CliConnection, args []string) {
 	app := args[1]
-	mySpace, _ := cliConnection.GetCurrentSpace()
 
-	output, err := cliConnection.CliCommandWithoutTerminalOutput("curl", fmt.Sprintf("v2/apps?q=name:%s&q=space_guid:%s", app, mySpace.Guid))
+	appGuid, err := getAppGuid(cliConnection, app)
+	FreakOut(err)
+
+	output, err := cliConnection.CliCommandWithoutTerminalOutput("curl", fmt.Sprintf("v3/apps/%s/revisions", appGuid))
 	FreakOut(err)
 	response := stringifyCurlResponse(output)
-	apps := AppsModel{}
-	err = json.Unmarshal([]byte(response), &apps)
-	FreakOut(err)
-	if len(apps.Resources) == 0 {
-		fmt.Printf("App %s not found", app)
-		return
-	}
-	appGuid := apps.Resources[0].Metadata.Guid
-
-	output, err = cliConnection.CliCommandWithoutTerminalOutput("curl", fmt.Sprintf("v3/apps/%s/revisions", appGuid))
-	FreakOut(err)
-	response = stringifyCurlResponse(output)
 	revisions := RevisionsModel{}
 	err = json.Unmarshal([]byte(response), &revisions)
 	FreakOut(err)
 
-	table := NewTable([]string{"version","droplet"})
+	table := NewTable([]string{"version", "droplet"})
 	fmt.Printf("Displaying revisions for app %s\r\n\r\n", app)
 	for _, revision := range revisions.Resources {
-		table.Add(fmt.Sprintf("%v",revision.Version), revision.Droplet.Guid)
+		table.Add(fmt.Sprintf("%v", revision.Version), revision.Droplet.Guid)
 	}
 	table.Print()
+}
+
+func (c *CFRevisionsPlugin) showRevisionDetails(cliConnection plugin.CliConnection, args []string) {
+	app := args[1]
+	version := args[2]
+
+	appGuid, err := getAppGuid(cliConnection, app)
+	FreakOut(err)
+
+	output, err := cliConnection.CliCommandWithoutTerminalOutput("curl", fmt.Sprintf("v3/apps/%s/revisions?versions=%s", appGuid, version))
+	FreakOut(err)
+	response := stringifyCurlResponse(output)
+	revisions := RevisionsModel{}
+	err = json.Unmarshal([]byte(response), &revisions)
+	FreakOut(err)
+
+	revision := revisions.Resources[0]
+
+	fmt.Printf("Displaying revision details for revision %v of app %s\r\n\r\n", version, app)
+	fmt.Printf("version: %v\r\n", revision.Version)
+	fmt.Printf("droplet: %s\r\n", revision.Droplet.Guid)
+}
+
+func getAppGuid(cliConnection plugin.CliConnection, app string) (appGuid string, err error) {
+	mySpace, _ := cliConnection.GetCurrentSpace()
+
+	output, err := cliConnection.CliCommandWithoutTerminalOutput("curl", fmt.Sprintf("v2/apps?q=name:%s&q=space_guid:%s", app, mySpace.Guid))
+	if err != nil {
+		return
+	}
+	response := stringifyCurlResponse(output)
+
+	apps := AppsModel{}
+	err = json.Unmarshal([]byte(response), &apps)
+	if (err != nil) {
+		return
+	}
+
+	if len(apps.Resources) == 0 {
+		err = errors.New(fmt.Sprintf("App %s not found", app))
+		return
+	}
+
+	appGuid = apps.Resources[0].Metadata.Guid
+	return
 }
 
 func stringifyCurlResponse(output []string) string {
